d1ea16b05865d8d121c76232ef931b5bdd72faf0
turkey-kim <rlaxmrgml@naver.com>
2023-10-02 17:56:47 +0900

    fix: credentails 추가

--- aws.js
+++ aws.js
@@ -3,7 +3,9 @@ dotenv.config();
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  rregion: "ap-northeast-2",
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
